9858cdddbffa6b14dc50b750ab0659ccf90e409c
charlotteflygare <38113960+charlotteflygare@users.noreply.github.com>
2018-10-25 11:49:08 +0200

    Acessibility

--- code/script.js
+++ code/script.js
@@ -4,6 +4,6 @@ function toggle() {
 }
 
 // Selects and HTML element, and calls a function which will be executed when the element is clicked.
-document.getElementById("section1").onclick = toggle
-document.getElementById("section2").onclick = toggle
-document.getElementById("section3").onclick = toggle
+document.getElementById("headingOne").onclick = toggle
+document.getElementById("headingTwo").onclick = toggle
+document.getElementById("headingThree").onclick = toggle
